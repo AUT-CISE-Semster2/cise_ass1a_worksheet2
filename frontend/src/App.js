7187cb8d3ba2675c222d3a242bbdba466ffdc16a
Travish <83637066+TravisPDC@users.noreply.github.com>
2021-08-01 17:29:06 +1200

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to CISE - the home of learning and fun</h1>
+     <h1>Welcome to CISE - the home of learning and fun</h1>
     </div>
   );
 }
